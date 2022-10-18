317ccd6f86c054476a9529a96595a04651d5f2b8
sivauerbach <114685221+sivauerbach@users.noreply.github.com>
2022-10-17 19:36:51 -0500

    Add files via upload

---
+++ Group_05/Lab_3/Lab3EX3A.cpp
@@ -0,0 +1,145 @@
+//Use g++ -std=c++11 -o Lab3EX3A Lab3EX3A.cpp -lwiringPi
+
+#include <iostream>
+#include <iomanip>
+#include <unistd.h>
+#include <cstdlib>
+#include <stdio.h>
+#include <string.h>
+#include <string>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <wiringSerial.h>
+#include <wiringPi.h>
+#define PORT 8080
+using namespace std;
+
+void movement(int, int);
+void createSocket();
+void readData();
+
+int kobuki, new_socket;
+
+/*Create char buffer to store transmitted data*/
+char buffer[1024] = {0};
+
+int main(){
+	//Initialize filestream for the Kobuki
+	wiringPiSetup();
+	kobuki = serialOpen("/dev/kobuki", 115200);
+
+	//Create connection to client
+	createSocket();
+
+	while(true){
+		//Read data from client
+		readData();
+	}
+	return 0;
+}
+
+void movement(int sp, int r){
+
+	//Create the byte stream packet with the following format:
+	unsigned char b_0 = 0xAA; /*Byte 0: Kobuki Header 0*/
+	unsigned char b_1 = 0x55; /*Byte 1: Kobuki Header 1*/
+	unsigned char b_2 = 0x06; /*Byte 2: Length of Payload*/
+	unsigned char b_3 = 0x01; /*Byte 3: Sub-Payload Header*/
+	unsigned char b_4 = 0x04; /*Byte 4: Length of Sub-Payload*/
+	unsigned char b_5 = sp & 0xff;	//Byte 5: Payload Data: Speed(mm/s)
+	unsigned char b_6 = (sp >> 8) & 0xff; //Byte 6: Payload Data: Speed(mm/s)
+	unsigned char b_7 = r & 0xff;	//Byte 7: Payload Data: Radius(mm)
+	unsigned char b_8 = (r >> 8) & 0xff;	//Byte 8: Payload Data: Radius(mm)
+	unsigned char checksum = 0;		//Byte 9: Checksum
+	
+	//Checksum all of the data
+	char packet[] = {b_0,b_1,b_2,b_3,b_4,b_5,b_6,b_7,b_8};
+	for (unsigned int i = 2; i < 9; i++)
+		checksum ^= packet[i];
+
+	/*Send the data (Byte 1 - Byte 9) to Kobuki using serialPutchar (kobuki, );*/
+	char send[] ={b_0,b_1,b_2,b_3,b_4,b_5,b_6,b_7,b_8,};
+	
+	for (unsigned int i = 0; i < 9; i++)
+		serialPutchar(kobuki, send[i]);
+
+	/*Pause the script so the data send rate is the
+	same as the Kobuki data receive rate*/
+	usleep(20000);
+}
+
+//Creates the connection between the client and
+//the server with the Kobuki being the server
+void createSocket(){
+	int server_fd;
+	struct sockaddr_in address;
+	int opt =1;
+	int addrlen = sizeof(address);
+
+	if((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0){
+		perror("socket failed");
+		exit(EXIT_FAILURE);
+	}
+
+	if(setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt))){
+		perror("setsockopt");
+		exit(EXIT_FAILURE);
+	}
+
+	address.sin_family      = AF_INET;
+	address.sin_addr.s_addr = INADDR_ANY;
+	address.sin_port        = htons(PORT);
+
+	if(bind(server_fd, (struct sockaddr *)&address, sizeof(address)) < 0){
+		perror("bind failed");
+		exit(EXIT_FAILURE);
+	}
+
+	if(listen(server_fd, 3) < 0){
+		perror("listen");
+		exit(EXIT_FAILURE);
+	}
+
+	if((new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t*)&addrlen)) < 0){
+		perror("accept");
+		exit(EXIT_FAILURE);
+	}
+}
+
+void readData(){
+	int valread;
+	
+	/*Read the incoming data stream from the controller*/
+	valread = read(new_socket, buffer, 1024);
+
+	
+	
+	/*Print the data to the terminal*/
+	printf("%s\n",buffer); 
+
+	
+
+	/*Use the received data to control the Kobuki*/
+
+	
+	if(false) 
+	{
+	/*Closes out of all connections cleanly*/
+
+	//When you need to close out of all connections, please
+	//close both the Kobuki and TTP/IP data streams.
+	//Not doing so will result in the need to restart
+	//the raspberry pi and Kobuki
+		close(new_socket);
+		serialClose(kobuki);
+		exit(0);
+	}
+
+
+
+
+	/*Reset the buffer*/
+	memset(&buffer, '0', sizeof(buffer));
+
+
+}
---
+++ Group_05/Lab_3/Lab3EX3B.cpp
@@ -0,0 +1,132 @@
+//Use g++ joystick.cc -std=c++11 -o Lab3EX3B Lab3EX3B.cpp
+
+#include <stdio.h>
+#include <iostream>
+#include <sys/socket.h>
+#include <stdlib.h>
+#include <netinet/in.h>
+#include <string.h>
+#include <arpa/inet.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <string>
+#include "joystick.hh"
+#include <cstdlib>
+
+#define  PORT 8080
+using namespace std;
+
+int createSocket();
+
+int sock = 0;
+
+int main(int argc, char const *argv[]){
+	
+	//Open the file stream for the joystick
+	Joystick joystick("/dev/input/js0");
+	JoystickEvent event;
+	if(!joystick.isFound()){
+		cout << "Error opening joystick" << endl;
+		exit(1);
+	}
+
+
+	//Create the connection to the server
+	createSocket();
+
+	while(true){
+
+		/*Convert the event to a useable data type so it can be sent*/
+		char send_string;
+
+		/*Sample the events from the joystick*/
+		if (!joystick.sample(&event)) continue;
+
+		if (event.isButton())
+			{
+				printf("isButton: %u | Value: %d\n", event.number, event.value);
+				/*Interpret the joystick input and use that input to move the Kobuki*/
+				
+				//START button
+				if ((event.number==7)&&(event.value==1)) send_string ='s';
+				
+				//Logitech button is pressed
+				//exit the script and close the Kobuki's connection cleanly
+				else if ((event.number==8)&&(event.value)) {
+					send_string='q';
+				}
+			}
+			if (event.isAxis())
+			{
+				printf("isAxis: %u | Value: %d\n", event.number, event.value);
+
+				//D-Pad Left/Right
+				if (event.number==6){
+					if (event.value > 100) send_string='r';
+					else if (event.value < -100) send_string='l';
+				}
+
+				// D-Pad Up/Down
+				if (event.number==7) {
+					if (event.value < -100) send_string='u';
+					else if (event.value > 100) send_string='d';
+			}
+
+
+		
+
+		/*Print the data stream to the terminal*/
+		cout<< "Send String: "<<send_string<<endl;
+		/*Send the data to the server*/
+		
+		send(sock , &send_string , strlen(&send_string) , 0);
+
+		if(event.isButton() && (event.number==8)) {
+		/*Closes out of all connections cleanly*/
+		send(sock , "q" , strlen(&send_string) , 0);
+
+		//When you need to close out of the connection, please
+		//close TTP/IP data streams.
+		//Not doing so will result in the need to restart
+		//the raspberry pi and Kobuki
+			close(sock);
+			exit(0);
+
+		/*Set a delay*/
+		usleep(200e3);
+		}
+	}
+	return 0;
+}
+}
+
+//Creates the connection between the client and
+//the server with the controller being the client
+
+int createSocket() {
+	struct sockaddr_in address;
+	struct sockaddr_in serv_addr;
+
+	if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0){
+		printf("\nSocket creation error \n");
+		return -1;
+	}
+
+	memset(&serv_addr, '0', sizeof(serv_addr));
+
+	serv_addr.sin_family = AF_INET;
+	serv_addr.sin_port   = htons(PORT);
+
+	/*Use the IP address of the server you are connecting to*/
+	if(inet_pton(AF_INET, "XX.XX.XX.XX" , &serv_addr.sin_addr) <= 0){
+		printf("\nInvalid address/ Address not supported \n");
+		return -1;
+	}
+
+	if(connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0){
+		printf("\nConnection Failed \n");
+		return -1;
+	}
+	return 0;
+}
+
