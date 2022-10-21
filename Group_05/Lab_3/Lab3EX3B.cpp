fdba9929e57480bd74484f8c549071c487de9977
sivauerbach <114685221+sivauerbach@users.noreply.github.com>
2022-10-21 18:12:36 -0500

    Friday Changes
    Works, can clean up print statements.

--- Group_05/Lab_3/Lab3EX3B.cpp
+++ Group_05/Lab_3/Lab3EX3B.cpp
@@ -19,7 +19,7 @@ using namespace std;
 int createSocket();
 
 int sock = 0;
-
+//CLIENT SIDE
 int main(int argc, char const *argv[]){
 	
 	//Open the file stream for the joystick
@@ -33,71 +33,67 @@ int main(int argc, char const *argv[]){
 
 	//Create the connection to the server
 	createSocket();
-
+	bool quitFlag= false;
 	while(true){
 
+		/*Set a delay*/
+		usleep(20000);
 		/*Convert the event to a useable data type so it can be sent*/
 		char send_string;
 
 		/*Sample the events from the joystick*/
 		if (!joystick.sample(&event)) continue;
 
-		if (event.isButton())
-			{
-				printf("isButton: %u | Value: %d\n", event.number, event.value);
-				/*Interpret the joystick input and use that input to move the Kobuki*/
-				
-				//START button
-				if ((event.number==7)&&(event.value==1)) send_string ='s';
-				
-				//Logitech button is pressed
-				//exit the script and close the Kobuki's connection cleanly
-				else if ((event.number==8)&&(event.value)) {
-					send_string='q';
-				}
+		if (event.isButton() && event.value) {
+			printf("isButton: %u | Value: %d\n", event.number, event.value);
+			/*Interpret the joystick input and use that input to move the Kobuki*/
+			
+			//START button
+			if ((event.number==7)&&(event.value==1)) send_string ='s';
+			
+			//Logitech button is pressed
+			//exit the script and close the Kobuki's connection cleanly
+			else if ((event.number==8)&&(event.value)) {
+				send_string='q';
+				quitFlag=true;
 			}
-			if (event.isAxis())
-			{
-				printf("isAxis: %u | Value: %d\n", event.number, event.value);
-
-				//D-Pad Left/Right
-				if (event.number==6){
-					if (event.value > 100) send_string='r';
-					else if (event.value < -100) send_string='l';
-				}
-
-				// D-Pad Up/Down
-				if (event.number==7) {
-					if (event.value < -100) send_string='u';
-					else if (event.value > 100) send_string='d';
+		}
+		if (event.isAxis()) {
+			printf("isAxis: %u | Value: %d\n", event.number, event.value);
+			//D-Pad Left/Right
+			if (event.number==6){
+				if (event.value > 100) send_string='r';
+				else if (event.value < -100) send_string='l';
 			}
 
+			// D-Pad Up/Down
+			if (event.number==7) {
+				if (event.value < -100) send_string='u';
+				else if (event.value > 100) send_string='d';
+			}
+		}
 
-		
-
+	
 		/*Print the data stream to the terminal*/
 		cout<< "Send String: "<<send_string<<endl;
 		/*Send the data to the server*/
 		
 		send(sock , &send_string , strlen(&send_string) , 0);
 
-		if(event.isButton() && (event.number==8)) {
 		/*Closes out of all connections cleanly*/
-		send(sock , "q" , strlen(&send_string) , 0);
-
 		//When you need to close out of the connection, please
 		//close TTP/IP data streams.
 		//Not doing so will result in the need to restart
 		//the raspberry pi and Kobuki
+		if(quitFlag) {
+			send(sock , "q" , strlen(&send_string) , 0);
 			close(sock);
 			exit(0);
-
-		/*Set a delay*/
-		usleep(200e3);
 		}
+		/*Set a delay*/
+		usleep(20000);
 	}
 	return 0;
-}
 }
 
 //Creates the connection between the client and
@@ -118,7 +114,7 @@ int createSocket() {
 	serv_addr.sin_port   = htons(PORT);
 
 	/*Use the IP address of the server you are connecting to*/
-	if(inet_pton(AF_INET, "XX.XX.XX.XX" , &serv_addr.sin_addr) <= 0){
+	if(inet_pton(AF_INET, "127.0.0.1" , &serv_addr.sin_addr) <= 0){
 		printf("\nInvalid address/ Address not supported \n");
 		return -1;
 	}
